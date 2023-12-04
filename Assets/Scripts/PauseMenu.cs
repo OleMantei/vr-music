0cddc32d2e1539dfb54d859543ac5d3c111b9b35
Tristan <tristan.baeumer@stud.hshl.de>
2023-12-04 04:05:29 +0100

    UI basics, no scene

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause"))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+        if (GamePaused)
+        {
+            if (Input.GetKeyDown("r"))//Reload"))
+            {
+                ReLoadScene();
+            }
+
+            if (Input.GetKeyDown("q"))//Cancel"))
+            {
+                QuitGame();
+            }
+        }
+
+        if (!GamePaused)
+        {
+            PauseMenuUI.SetActive(false);
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        //Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        GamePaused = true;
+    }
+
+    public void ReLoadScene()
+    {
+        //Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
